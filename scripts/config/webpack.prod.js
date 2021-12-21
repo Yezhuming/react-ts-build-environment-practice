b9d07ad55d5a8840decbe325506294a8ec64edac
Ye <3011852376@qq.com>
2021-12-21 21:18:01 +0800

    chore: 新增去除无用样式打包功能

--- scripts/config/webpack.prod.js
+++ scripts/config/webpack.prod.js
@@ -1,8 +1,12 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const glob = require('glob');
+const PurgeCSSPlugin = require('purgecss-webpack-plugin');
 const common = require('./webpack.common');
+const { PROJECT_PATH } = require('../constant');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -17,11 +21,16 @@ module.exports = merge(common, {
       ignoreOrder: false,
     }),
     // 包分析器
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
-      analyzerPort: 8091, // 运行后的端口号 可以修改
-      generateStatsFile: true,
-      statsOptions: { source: false },
+    // new BundleAnalyzerPlugin({
+    //   analyzerMode: 'static',
+    //   analyzerPort: 8091, // 运行后的端口号 可以修改
+    //   generateStatsFile: true,
+    //   statsOptions: { source: false },
+    // }),
+    // 去除无用样式
+    new PurgeCSSPlugin({
+      paths: glob.sync(`${path.resolve(PROJECT_PATH, './src')}/**/*.{tsx,scss,less,css}`, { nodir: true }),
+      whitelist: ['html', 'body'],
     }),
   ],
 });
